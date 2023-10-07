9163391f345fe9aae7184b7479fa1e808e15ae97
TamiraAtieno1 <tamiraatieno20@gmail.com>
2023-10-07 04:41:37 +0300

    new updates

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path 
-from .views import getdata
+from django.urls import path, include
+from .views import getdata,addData,updateData
 urlpatterns = [
     path('', getdata, name="getdata"),
+    path('add/', addData, name="addData"),
+    path('update/', updateData, name="updateData")
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,35 @@
 from rest_framework.decorators import api_view
+from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from blogapp.models import Blog2
 from .serializers import Blog2Serializer
+
 @api_view(["GET"])
 def getdata(request):
     blogs = Blog2.objects.all()
     serializers = Blog2Serializer(blogs, many=True)
     return Response(serializers.data)
-    
+
+
+@api_view(["POST"])
+def addData(request):
+    serializer = Blog2Serializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save() 
+    return Response(serializer.data)
+
+
+@api_view(["PATCH"])
+def updateData(request):
+    blogs = Blog2.objects.get(id=id)
+    serializer = Blog2Serializer(result, data = request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save() 
+    return Response(serializer.data)
+
+              
+
+              
+
+
